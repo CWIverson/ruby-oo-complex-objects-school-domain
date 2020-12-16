2d2756633404f20b4ae1d0a7ecc5a6933c691b03
christian iverson <chris.w.iverson@gmail.com>
2020-12-16 12:58:04 -0800

    commit tree 093cd4cbbe4ba0de9666057288f0319fc6010948

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def add_student(new_student, grade)
+        if !@roster.key?(grade)
+            @roster[grade]=[new_student]
+        else
+            @roster[grade]<< new_student
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.map{|k,v| [k,v.sort]}.sort.to_h
+    end
+end 
